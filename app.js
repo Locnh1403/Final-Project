af6685f0228ecc75e1f94b80fa95caeb94f69c87
Locnh1403 <locnh1403@gmail.com>
2022-04-08 22:53:14 +0700

    push

---
+++ app.js
@@ -0,0 +1,65 @@
+const menuLinks = document.querySelector('.navbar__menu');
+const navLogo = document.querySelector('#navbar__logo');
+const body = document.querySelector('body');
+
+//Animations
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from('.animate-reg', {
+    duration: 0.6,
+    opacity: 0,
+    y: -150,
+    stagger: 0.3
+});
+
+gsap.from('.animate-services', {
+    scrollTrigger: '.animate-services',
+    duration: 0.6,
+    opacity: 1,
+    x: -150,
+    stagger: 0.1
+});
+
+gsap.from('.animate-img', {
+    scrollTrigger: '.animate-services',
+    duration: 1.2,
+    opacity: 0,
+    x: -200,
+});
+
+gsap.from('.animate-membership', {
+    scrollTrigger: '.animate-membership',
+    duration: 1,
+    opacity: 0,
+    y: -150,
+    stagger: 0.3,
+    delay: 0.5
+});
+
+gsap.from('.animate-card', {
+    scrollTrigger: '.animate-card',
+    duration: 1,
+    opacity: 0,
+    y: -150,
+    stagger: 0.1,
+    delay: 0.2
+});
+
+gsap.from('.animate-team', {
+    scrollTrigger: '.animate-team',
+    duration: 1,
+    opacity: 0,
+    y: -150,
+    stagger: 0.3,
+    delay: 0.2
+});
+
+gsap.from('.animate-email', {
+    scrollTrigger: '.animate-email',
+    duration: 0.8,
+    opacity: 0,
+    y: -150,
+    stagger: 0.25,
+    delay: 0.4
+});
+
